6b4dfbc4a5327bc4628445c1f3323b19ec292164
kametarou <20429816+kametarou@users.noreply.github.com>
2021-03-26 09:29:57 +0900

    Update densuke.py
    Exit if the number of arguments is not appropriate.

--- densuke.py
+++ densuke.py
@@ -9,6 +9,8 @@ youbi={0:'(月)', 1:'(火)',2:'(水)',3:'(木)',4:'(金)',5:'(土)',6:'(日)'}
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("usage: python densuke.py yyyy m")
+        sys.exit(1)
+        
     cal=calendar.Calendar(SUNDAY) # 日曜始まりのカレンダーを生成
     year=sys.argv[1]
     month=sys.argv[2]
